a83aa4e00c3becf06d854d88d5b8f071b247440f
Michael Sowah <sowahm808@gmail.com>
2026-06-27 21:46:04 -0500

    updated main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import 'zone.js';
 import { ErrorHandler } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, RouteReuseStrategy } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
+
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -18,10 +19,16 @@ import { AppErrorHandler } from './app/services/app-error-handler.service';
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: ErrorHandler, useClass: AppErrorHandler },
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideRouter(appRoutes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+
     provideIonicAngular(),
+
+    provideRouter(
+      appRoutes,
+      withEnabledBlockingInitialNavigation(),
+    ),
+
+    provideHttpClient(withInterceptors([authInterceptor])),
+
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
