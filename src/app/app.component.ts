eea297f0d9f28ec2c641b43cf171c974017e0eb7
Michael Sowah <sowahm808@gmail.com>
2026-06-30 16:04:52 -0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterOutlet } from '@angular/router';
+import { IonApp } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, RouterOutlet],
   template: `
     <ion-app>
-      <ion-router-outlet></ion-router-outlet>
+      <router-outlet></router-outlet>
     </ion-app>
   `,
 })
